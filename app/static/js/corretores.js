e45be1be084a556eae89d8a632d4ae8e61fafc74
TechFring <danilo030920@gmail.com>
2020-10-27 13:18:17 -0300

    adicionei a confirmação ao apagar um registro

--- app/static/js/corretores.js
+++ app/static/js/corretores.js
@@ -6,3 +6,11 @@ function gerarSenha() {
   label.focus();
   senha.value = senhaAleatoria;
 }
+
+function apagarCorretor(id) {
+  const confirmar = confirm('Tem certeza que deseja apagar esse registro?')
+
+  if (confirmar === true) {
+    window.location.href = `/corretores/apagar/${id}`
+  }
+}
